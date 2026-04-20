8f02847a35a234332d2513f0a9bb33af6612fafd
Caleb Croff <ccroff@edustaff.org>
2026-04-20 14:08:07 -0400

    Add auth export to firebase.ts for Google Sign-In support

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,18 @@
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  //   // COPY this from your Firebase Console
-  //   apiKey: "your-api-key-goes-here",
-  //   authDomain: "your-project-name-here.firebaseapp.com",
-  //   databaseURL: "https://your-project-name-here.firebaseio.com",
-  //   projectId: "your-project-name-here",
-  //   storageBucket: "your-project-name.appspot.com",
-  //   messagingSenderId: "xxxxxxxx",
+  apiKey: "AIzaSyCuMOEw0R1OVkvOS0iGBNTWxJxMjlS6B9w",
+  authDomain: "beverageshop-1970.firebaseapp.com",
+  projectId: "beverageshop-1970",
+  storageBucket: "beverageshop-1970.firebasestorage.app",
+  messagingSenderId: "749114148434",
+  appId: "1:749114148434:web:a086c037f418662069c18b",
+  measurementId: "G-M9M0Y01ZF8",
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
+export const db = getFirestore(app);
+export const auth = getAuth(app);
 export default db;
