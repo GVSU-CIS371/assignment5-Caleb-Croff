8e7dc6b8e94b9bedc47b838dbacfaef2f2823262
Caleb Croff <ccroff@edustaff.org>
2026-04-20 14:08:38 -0400

    Call store.init() before mounting app to load Firestore data first

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from "vue";
 import "./styles/mug.scss";
 import { createPinia } from "pinia";
-import piniaPluginPersistedState from "pinia-plugin-persistedstate";
 import App from "./App.vue";
-const pinia = createPinia();
-pinia.use(piniaPluginPersistedState);
+import { useBeverageStore } from "./stores/beverageStore";
 
-createApp(App).use(pinia).mount("#app");
+const pinia = createPinia();
+const app = createApp(App).use(pinia);
+
+const store = useBeverageStore();
+store.init().then(() => {
+  app.mount("#app");
+});
