c99fb5037bc0c21508eb8770882f006b5e5327d5
Caleb Croff <ccroff@edustaff.org>
2026-04-20 14:08:59 -0400

    Add Google Sign-In, sign out, auth state display, and saved beverages UI

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <Beverage :isIced="beverageStore.currentTemp === 'Cold'" />
+
+    <div id="auth-area">
+      <div v-if="beverageStore.user">
+        <span>{{ beverageStore.user.displayName || beverageStore.user.email }}</span>
+        <button type="button" @click="signOut">Sign Out</button>
+      </div>
+      <button v-else type="button" @click="withGoogle">Sign in with Google</button>
+    </div>
+
+    <p v-if="message" id="message">{{ message }}</p>
+
     <ul>
       <li>
         <template v-for="temp in beverageStore.temps" :key="temp">
@@ -59,6 +70,7 @@
         </template>
       </li>
     </ul>
+
     <input
       id="beverage-name"
       v-model="beverageStore.beverageName"
@@ -66,25 +78,57 @@
       aria-label="Beverage Name"
       placeholder="Beverage Name"
     />
-    <button type="button" @click="beverageStore.makeBeverage()">Make Beverage</button>
-    <div id="beverage-container">
-      <button
-        v-for="beverage in beverageStore.beverages"
-        :key="beverage.id"
-        type="button"
-        @click="beverageStore.showBeverage(beverage.id)"
-      >
-        {{ beverage.name }}
-      </button>
+    <button type="button" :disabled="!beverageStore.user" @click="makeBeverage">Make Beverage</button>
+
+    <div v-if="beverageStore.user && beverageStore.beverages.length" id="beverage-container">
+      <template v-for="beverage in beverageStore.beverages" :key="beverage.id">
+        <label>
+          <input
+            type="radio"
+            name="saved-beverage"
+            :value="beverage.id"
+            v-model="beverageStore.selectedBeverageId"
+            @change="beverageStore.showBeverage(beverage.id)"
+          />
+          {{ beverage.name }}
+        </label>
+      </template>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+import { GoogleAuthProvider, signInWithPopup, signOut as firebaseSignOut, onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase.ts";
 import Beverage from "./components/Beverage.vue";
 import { useBeverageStore } from "./stores/beverageStore";
 
 const beverageStore = useBeverageStore();
+const message = ref("");
+
+onAuthStateChanged(auth, (user) => {
+  beverageStore.setUser(user);
+});
+
+async function withGoogle() {
+  try {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider);
+    message.value = "";
+  } catch (err: any) {
+    message.value = err.message ?? "Sign-in failed. Please try again.";
+  }
+}
+
+async function signOut() {
+  await firebaseSignOut(auth);
+  message.value = "";
+}
+
+async function makeBeverage() {
+  message.value = await beverageStore.makeBeverage();
+}
 </script>
 
 <style lang="scss">
